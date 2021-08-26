04e3b4fd05afc8895d89bf7f09d7ef6ebd274b96
myrizkyprasetya <ifharizky28@gmail.com>
2021-08-26 21:17:52 +0700

    update pengisian place order

--- cypress/integration/Study-Case3/pembelian_barang.js
+++ cypress/integration/Study-Case3/pembelian_barang.js
@@ -5,25 +5,28 @@ describe('test case pembelian barang', function(){
 
     it('tc-01', function(){
         //proses pembelian barang
-        cy.wait(4000)
+        cy.wait(2000)
         
         cy.get(':nth-child(6) > .card > .card-block > .card-title > .hrefch').should('have.text', 'Sony xperia z5').click()
-        cy.wait(4000)
+        cy.wait(3000)
 
         cy.get('.col-sm-12 > .btn').should('have.text', 'Add to cart').click()
-        cy.wait(4000)
+        cy.wait(3000)
 
         cy.get(':nth-child(4) > .nav-link').should('have.text', 'Cart').click()
-        cy.wait(4000)
+        cy.wait(3000)
 
         cy.get('.col-lg-1 > .btn').should('have.text', 'Place Order').click()
-        cy.wait(4000)
+        cy.wait(2000)
         
         //mengisi place order       
         cy.get('#name').type('Luoyi').should('have.value', 'Luoyi');
         cy.get('#country').type('Japan').should('have.value', 'Japan');
         cy.get('#city').type('Osaka').should('have.value', 'Osaka');
         cy.get('#card').type('62312345667').should('have.value', '62312345667');
+        cy.get('#month').type('August').should('have.value', 'August');
+        cy.get('#year').type('2021').should('have.value', '2021');
+        cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').should('have.class', 'btn btn-primary').click()
        
     })
 
