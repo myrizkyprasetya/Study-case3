253a7820c63c0e08f9aeaae52ba0478b8b018ed2
myrizkyprasetya <ifharizky28@gmail.com>
2021-08-28 22:49:07 +0700

    updating code login.js

--- cypress/integration/Study-Case3/login.js
+++ cypress/integration/Study-Case3/login.js
@@ -1,12 +1,16 @@
 describe('test case login', function(){
-    beforeEach(()=>{
+    before(()=>{
         cy.visit('https://www.demoblaze.com/index.html')
     })
 
-    it('tc-01', function(){        
-        cy.get('#et_pb_contact_name_0').type('Yasifa').should('have.value', 'Yasifa');
-        cy.get('#et_pb_contact_email_0').type('ifharizky@gmail.com').should('have.value', 'ifharizky@gmail.com');
-        cy.get('.et_pb_contact_submit').should('have.text', 'Email Me!').click()
+    it('tc-login01', function(){        
+        //proses login
+        cy.get('#login2').should('have.id', 'login2').click()
+        cy.get('#loginusername').type('dojo-Luoyi')
+        cy.get('#loginusername').should('have.value', 'dojo-Luoyi')
+        cy.get('#loginpassword').type('dojo-luoyi')
+        cy.get('#loginpassword').should('have.value', 'dojo-luoyi')
+        cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').should('have.class', 'btn btn-primary').click()
     })
 
     Cypress.on('uncaught:exception', (err, runnable)=>{
